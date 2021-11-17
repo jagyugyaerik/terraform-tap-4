69c6789f6f4e5949eea037ac10bb12755cffe1de
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-11-17 14:41:18 +0100

    Format vpc profile
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- profiles/s3-backend/outputs.tf
+++ profiles/s3-backend/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_id" {
-    value = module.s3_backend.bucket_id
+  value = module.s3_backend.bucket_id
 }
 
 output "dynamodb_table_name" {
-    value = module.s3_backend.dynamodb_table_name
+  value = module.s3_backend.dynamodb_table_name
 }
