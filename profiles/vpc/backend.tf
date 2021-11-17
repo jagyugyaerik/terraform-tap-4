e3ba74a543ab171f2b7f9760e7c65dd81cc9a62f
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-11-17 14:46:33 +0100

    Format profile vpc
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- profiles/vpc/backend.tf
+++ profiles/vpc/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "eja2-197996810626-backend"
-    key = "terraform/lab2/vpc.tfstate"
-    region = "eu-north-1"
+    bucket         = "eja2-197996810626-backend"
+    key            = "terraform/lab2/vpc.tfstate"
+    region         = "eu-north-1"
     dynamodb_table = "eja2-backend"
-    encrypt = "true"
+    encrypt        = "true"
   }
   # required_version = "~> 0.12.0"
 }
